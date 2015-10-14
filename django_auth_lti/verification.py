655463b41be29f0a64ca1a30077492912549cb19
Jill Vogel <jill.vogel@adelaide.edu.au>
2015-10-14 12:14:47 +1030

    ADX-192 Merge upstream changes from django-auth-lti.

--- django_auth_lti/verification.py
+++ django_auth_lti/verification.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 
+
 def is_allowed(request, allowed_roles, raise_exception):
     # allowed_roles can either be a string (for just one)
     # or a tuple or list (for several)
@@ -7,16 +8,16 @@ def is_allowed(request, allowed_roles, raise_exception):
         allowed = (allowed_roles, )
     else:
         allowed = allowed_roles
-    
-    lti_launch = request.session.get('LTI_LAUNCH', None)
-    if not isinstance(lti_launch, dict):
-        # If this is raised, then likely the project doesn't have
-        # the correct settings or is being run outside of an lti context
-        raise ImproperlyConfigured("No LTI_LAUNCH vale found in session")
-    user_roles = lti_launch.get('roles', [])
-    is_user_allowed =  set(allowed) & set(user_roles)
+
+    user_roles = request.LTI.get('roles', [])
+    is_user_allowed = set(allowed) & set(user_roles)
     
     if not is_user_allowed and raise_exception:
         raise PermissionDenied
     
     return is_user_allowed
+
+
+def has_lti_roles(request, roles):
+    user_roles = request.LTI.get('roles', [])
+    return bool(set(user_roles) & set(roles))
